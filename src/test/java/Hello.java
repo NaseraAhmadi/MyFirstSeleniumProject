99c68d9abb18df9ccfccc664932d438d4e3e30ac
ahmadreshad84 <80494804+ahmadreshad84@users.noreply.github.com>
2021-05-22 08:17:40 -0700

    Added name String

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,2 +1,8 @@
 public class Hello {
+    public static void main(String[] args) {
+
+        int num = 500;
+        String name = "Richard";
+        System.out.println("name = " + name);
+    }
 }
